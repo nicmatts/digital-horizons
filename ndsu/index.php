3ec61213c6ab686995ceaab87e80fb73bc476db2
Nicole M <nicmatts@gmail.com>
2014-06-25 08:19:25 -0500

    initial commit

---
+++ ndsu/index.php
@@ -0,0 +1,96 @@
+<head>
+	<link href='http://fonts.googleapis.com/css?family=Dancing+Script' rel='stylesheet' type='text/css'>
+	<!--[if lt IE 10]>
+	<link rel="stylesheet" type="text/css" href="if-ie9-and-down.css" />
+	<![endif]-->
+</head>
+<body>
+
+<div class="landing-page">
+	<div class="landing-page-logo">
+		<img src="http://funwithmattsons.com/Archives_logo.png" alt="NDSU Archives logo">
+	</div>
+				
+	<div class="landing-page-content">
+		<div class="landing-page-main">
+			<div class="container container-ndsu-1">
+				<div class="flipper flipper-ndsu-1">
+					<a href="http://cdm16921.contentdm.oclc.org/cdm/landingpage/collection/archives">
+						<div class="front face">
+							<img src="http://cdm16921.contentdm.oclc.org/utils/ajaxhelper/?CISOROOT=uw&CISOPTR=697&action=2&DMSCALE=95&DMWIDTH=475&DMHEIGHT=305&DMX=0&DMY=0&DMTEXT=&DMROTATE=0">
+							<h4>Broadway in Fargo, 1910s</h4>
+						</div>
+						<div class="back face back-ndsu-1">
+							<p class="photo-back">See more historic photos of Fargo in our Photo Gallery collection</p>
+						</div>
+					</a>
+				</div>
+			</div>
+
+			<!-- <div class="container container-ndsu-2">
+				<a href="http://cdm16921.contentdm.oclc.org/cdm/landingpage/collection/archives">
+					<div class="flipper flipper-ndsu-2 back-ndsu-2">
+						<div class="front face">
+							<img src="http://cdm16921.contentdm.oclc.org/utils/ajaxhelper/?CISOROOT=archives&CISOPTR=464&action=2&DMSCALE=125&DMWIDTH=512&DMHEIGHT=447&DMX=0&DMY=0&DMTEXT=&DMROTATE=0">
+							<h4>Fashion show at NDAC, 1921</h4>
+						</div>
+						<div class="back face">
+							<p class="photo-back">View more from our "NDSU Captured on Glass" collection</p>
+						</div>
+					</div>
+				</a>
+			</div> -->
+			<h2>About the NDSU Archives</h2>
+
+			<p>The NDSU Institute for Regional Studies and University Archives is the special collections and archives department of the North Dakota State University Libraries. Its mission is to identify, collect, preserve and makes accessible rare, valuable and unique historical materials in support of the research and teaching missions of North Dakota State University. It also serves the citizens of North Dakota and scholars throughout the world.</p>
+
+			<p>The goal of the Institute for Regional Studies and University Archives is to make the public aware of the quality and depth of its collections and to encourage further research using these materials. Offering these public web-based collections helps further the libraries’ and university’s missions of making unique collections and scholarship available electronically for use by students, faculty and staff, educators, and the wider community.</p>
+
+			<h2>Online Collection</h2>
+			<p>As part of Digital Horizons, the Institute for Regional Studies and University Archives is pleased to make available a growing collection of digital materials showcasing North Dakota and the Northern Great Plains. We invite everyone to explore the region as seen through the eyes of the people who lived here in the late 19th century and into the 20th century. Presented is a growing sample of the more than 200,000 images that make up the photograph collections in the Institute for Regional Studies and University Archives. This sampling is only a snapshot of our total holdings. We invite you to visit the archives or contact us directly to learn more about our holdings.</p>
+
+			<h2>Collection Policy</h2>
+			<p>We invite you to become a partner with the Institute for Regional Studies and University Archives by sharing historical documents and photographs with us. Your support is vital in documenting North Dakota’s and NDSU’s past and present. The generosity of friends and NDSU alumni has allowed the Institute and University Archives to acquire, preserve and make available a vast array of historical documents regarding North Dakota State University and North Dakota. For further information consult the Donations link on our website.</p>
+		</div>
+
+		<div class="landing-page-sidebar">
+			<h3>Collections</h3>
+			<ul>
+			<li><a href="http://cdm16921.contentdm.oclc.org/cdm/landingpage/collection/ndsu-spiker">Allen Spiker German Russian Dialect Tapes</a></li>
+			<li><a href="http://cdm16921.contentdm.oclc.org/cdm/landingpage/collection/ndsu-olson">Cal Olson Photography</a></li>
+			<li><a href="http://cdm16921.contentdm.oclc.org/cdm/landingpage/collection/dakota-lith">Dakota Lithographs and Engravings</a></li>
+			<li><a href="http://cdm16921.contentdm.oclc.org/cdm/landingpage/collection/fpl">Fargo Public Library Collection</a></li>
+			<li><a href="http://cdm16921.contentdm.oclc.org/cdm/landingpage/collection/ndsu-eagles">For Eagles To Be Crows</a></li>
+			<li><a href="http://cdm16921.contentdm.oclc.org/cdm/landingpage/collection/ndsu-bolley">H. L. Bolley Photograph Collection</a></li>
+			<li><a href="http://cdm16921.contentdm.oclc.org/cdm/landingpage/collection/ndsu-korea">Korean War Propaganda Leaflets</a></li>
+			<li><a href="http://cdm16921.contentdm.oclc.org/cdm/landingpage/collection/archives">NDSU Captured on Glass</a></li>
+			<li><a href="http://cdm16921.contentdm.oclc.org/cdm/landingpage/collection/ndsu-docs">NDSU-NDAC Historic Documents</a></li>
+			<li><a href="http://cdm16921.contentdm.oclc.org/cdm/landingpage/collection/ndsu-npl">Nonpartisan Leader Cartoons</a></li>
+			<li><a href="http://cdm16921.contentdm.oclc.org/cdm/landingpage/collection/ndsu-histor">North Dakota Historic Documents</a></li>
+			<li><a href="http://cdm16921.contentdm.oclc.org/cdm/landingpage/collection/ndsu-voices">North Dakota Voices from the Past</a></li>
+			<li><a href="http://cdm16921.contentdm.oclc.org/cdm/landingpage/collection/uw">Photo Gallery - Images from the NDSU Institute for Regional Studies</a></li>
+			<li><a href="http://cdm16921.contentdm.oclc.org/cdm/landingpage/collection/ndsu-slsonp">St. Lukes School of Nursing Photograph Collection</a></li>
+			<li><a href="http://cdm16921.contentdm.oclc.org/cdm/landingpage/collection/ndsu-strand">Strand Studio Photograph Collection</a></li>
+			</ul>
+
+			<h3>Contact Information</h3>
+			NDSU Archives<br>
+			Libraries<br>
+			NDSU Dept. #2080<br>
+			P.O. Box 6050<br>
+			Fargo ND 58108-6050<br>
+			<br>
+			701-231-8914<br>
+			701-231-5632 (fax)<br>
+			<a href="mailto:NDSU.Library.Archives@ndsu.edu">NDSU.Library.Archives@ndsu.edu</a><br>
+			<a href="library.ndsu.edu/ndsuarchives">library.ndsu.edu/ndsuarchives</a>
+
+			<h3>Ordering information</h3>
+			Use our <a href="http://library.ndsu.edu/ndsuarchives/sites/default/files/ndsu-archives-order-form.pdf">order form</a> to order a photograph or request permission to publish.  Visit our <a href="http://library.ndsu.edu/ndsuarchives/prices-and-fees/">Duplication Fees and Policies page</a> for information on reproduction costs and policies.
+
+			<h3>Donations</h3>
+			<a href="http://library.ndsu.edu/ndsuarchives/donations">Learn more about how to make a donation to the NDSU Archives</a>.
+		</div>
+	</div>
+</div>
+</body>
