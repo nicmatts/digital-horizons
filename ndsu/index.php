c11fc79e142bec398beef5c9c6e2213b9ecb94aa
Nicole M <nicmatts@gmail.com>
2014-07-07 13:41:53 -0500

    Add Prairie Public

--- ndsu/index.php
+++ ndsu/index.php
@@ -14,8 +14,8 @@
 	<div class="landing-page-content">
 		<div class="landing-page-main">
 			<div class="container container-ndsu-1">
-				<div class="flipper flipper-ndsu-1">
-					<a href="http://cdm16921.contentdm.oclc.org/cdm/landingpage/collection/archives">
+				<a href="http://cdm16921.contentdm.oclc.org/cdm/landingpage/collection/archives">
+					<div class="flipper flipper-ndsu-1">
 						<div class="front face">
 							<img src="http://cdm16921.contentdm.oclc.org/utils/ajaxhelper/?CISOROOT=uw&CISOPTR=697&action=2&DMSCALE=95&DMWIDTH=475&DMHEIGHT=305&DMX=0&DMY=0&DMTEXT=&DMROTATE=0">
 							<h4>Broadway in Fargo, 1910s</h4>
@@ -23,21 +23,19 @@
 						<div class="back face back-ndsu-1">
 							<p class="photo-back">See more historic photos of Fargo in our Photo Gallery collection</p>
 						</div>
-					</a>
-				</div>
+					</div>
+				</a>
 			</div>
 
 			<h2>About the NDSU Archives</h2>
 
-			<p>The NDSU Institute for Regional Studies and University Archives is the special collections and archives department of the North Dakota State University Libraries. Its mission is to identify, collect, preserve and makes accessible rare, valuable and unique historical materials in support of the research and teaching missions of North Dakota State University. It also serves the citizens of North Dakota and scholars throughout the world.</p>
-
-			<p>The goal of the Institute for Regional Studies and University Archives is to make the public aware of the quality and depth of its collections and to encourage further research using these materials. Offering these public web-based collections helps further the libraries’ and university’s missions of making unique collections and scholarship available electronically for use by students, faculty and staff, educators, and the wider community.</p>
+			<p>The <a href="http://library.ndsu.edu/ndsuarchives">NDSU Archives</a>, which is comprised of the Institute for Regional Studies Archives and University Archives, is the special collections and archives department of the North Dakota State University Libraries. Its mission is to identify, collect, preserve, and make accessible unique historical materials in support of the research and teaching missions of North Dakota State University. It also serves the citizens of North Dakota and scholars throughout the world.</p>
 
 			<h2>Online Collection</h2>
-			<p>As part of Digital Horizons, the Institute for Regional Studies and University Archives is pleased to make available a growing collection of digital materials showcasing North Dakota and the Northern Great Plains. We invite everyone to explore the region as seen through the eyes of the people who lived here in the late 19th century and into the 20th century. Presented is a growing sample of the more than 200,000 images that make up the photograph collections in the Institute for Regional Studies and University Archives. This sampling is only a snapshot of our total holdings. We invite you to visit the archives or contact us directly to learn more about our holdings.</p>
+			<p>As part of Digital Horizons, the NDSU Archives is pleased to make available a growing collection of digital materials showcasing North Dakota and NDSU (formerly known as the North Dakota Agricultural College from 1890 to 1960). We invite everyone to explore the region as seen through the eyes of the people and students who lived here in the late 19th century and into the 20th century. Presented is a growing sample of the more than 700,000 images that make up the photograph collections in the Institute for Regional Studies Archives and University Archives. This sampling is only a snapshot of our total holdings. We invite you to <a href="https://www.google.com/maps/preview?q=3551+7th+Ave+N,+Fargo,+ND&hl=en&sll=46.883318,-96.835717&sspn=0.010868,0.026157&oq=3551+7th&hnear=3551+7th+Ave+N,+Fargo,+North+Dakota+58102&t=m&z=16">visit the archives</a> or <a href="http://library.ndsu.edu/ndsuarchives/contact-information">contact us</a> directly to learn more about our holdings.</p>
 
 			<h2>Collection Policy</h2>
-			<p>We invite you to become a partner with the Institute for Regional Studies and University Archives by sharing historical documents and photographs with us. Your support is vital in documenting North Dakota’s and NDSU’s past and present. The generosity of friends and NDSU alumni has allowed the Institute and University Archives to acquire, preserve and make available a vast array of historical documents regarding North Dakota State University and North Dakota. For further information consult the Donations link on our website.</p>
+			<p>We invite you to become a partner with NDSU Archives by sharing historical documents and photographs with us. Your support is vital in documenting North Dakota’s and NDSU’s past and present. The generosity of friends and NDSU alumni has allowed the NDSU Archives to acquire, preserve and make available a vast array of historical documents regarding North Dakota State University and North Dakota. For further information consult the <a href="http://library.ndsu.edu/ndsuarchives/donations">Donations link on our website</a>.</p>
 		</div>
 
 		<div class="landing-page-sidebar">
---
+++ prairie-public/index.php
@@ -0,0 +1,57 @@
+<head>
+	<link href='http://fonts.googleapis.com/css?family=Dancing+Script' rel='stylesheet' type='text/css'>
+	<!--[if lt IE 10]>
+	<link rel="stylesheet" type="text/css" href="if-ie9-and-down.css" />
+	<![endif]-->
+</head>
+<body>
+
+<div class="landing-page">
+	<div class="landing-page-logo">
+		<img src="http://funwithmattsons.com/Archives_logo.png" alt="NDSU Archives logo">
+	</div>
+				
+	<div class="landing-page-content">
+		<div class="landing-page-main">
+			<div class="container container-pp-1">
+				<a href="http://cdm16921.contentdm.oclc.org/cdm/landingpage/collection/archives">
+					<div class="flipper flipper-pp-1">
+						<div class="front face">
+							<img src="http://cdm16921.contentdm.oclc.org/utils/ajaxhelper/?CISOROOT=pp-store&CISOPTR=43&action=2&DMSCALE=125&DMWIDTH=512&DMHEIGHT=407&DMX=0&DMY=0&DMTEXT=&DMROTATE=0">
+							<h4></h4>
+						</div>
+						<div class="back face pp-back">
+							<p class="photo-back"></p>
+						</div>
+					</div>
+				</a>
+			</div>
+
+			<h2>About Prairie Public</h2>
+
+			<p></p>
+
+			<h2>Online Collection</h2>
+			<p></p>
+
+			<h2>Collection Policy</h2>
+			<p></p>
+		</div>
+
+		<div class="landing-page-sidebar">
+			<a href="http://library.ndsu.edu/ndsuarchives/sites/default/files/ndsu-archives-order-form.pdf" class="search_content_button" id="order">Order Form</a>
+			<h3>Collections</h3>
+			<ul>
+				<li></li>
+			</ul>
+
+			<h3>Contact Information</h3>
+			
+
+			<h3>Ordering information</h3>
+
+			<h3>Donations</h3>
+		</div>
+	</div>
+</div>
+</body>
