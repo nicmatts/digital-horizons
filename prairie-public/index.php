95af4762622cf5c0bcad9019515d7a52c5a6b33a
Nicole M <nicmatts@gmail.com>
2014-07-18 13:32:56 -0500

    Add Prairie Public info

--- prairie-public/index.php
+++ prairie-public/index.php
@@ -8,49 +8,53 @@
 
 <div class="landing-page">
 	<div class="landing-page-logo">
-		<img src="http://funwithmattsons.com/Archives_logo.png" alt="NDSU Archives logo">
+		<img src="http://library.ndsu.edu/static/dh-logos/PrairiePublicLogo.jpg" alt="Prairie Public Broadcasting logo">
 	</div>
 				
 	<div class="landing-page-content">
 		<div class="landing-page-main">
 			<div class="container container-pp-1">
-				<a href="http://cdm16921.contentdm.oclc.org/cdm/landingpage/collection/archives">
+				<a href="http://cdm16921.contentdm.oclc.org/cdm/landingpage/collection/pp-store">
 					<div class="flipper flipper-pp-1">
 						<div class="front face">
 							<img src="http://cdm16921.contentdm.oclc.org/utils/ajaxhelper/?CISOROOT=pp-store&CISOPTR=43&action=2&DMSCALE=125&DMWIDTH=512&DMHEIGHT=407&DMX=0&DMY=0&DMTEXT=&DMROTATE=0">
-							<h4></h4>
+							<h4>Erling Rolfsrud on the set of Red River Land</h4>
 						</div>
 						<div class="back face pp-back">
-							<p class="photo-back"></p>
+							<p class="photo-back">See more from behind the scenes at Prairie Public.</p>
 						</div>
 					</div>
 				</a>
 			</div>
 
-			<h2>About Prairie Public</h2>
+			<h2>About Prairie Public Broadcasting</h2>
 
-			<p></p>
+			<p>Prairie Public Broadcasting is a trusted public service dedicated to building an exciting and productive future for the prairie and its people. Prairie Public offers a window on the world through national and regional television and radio programming; creates a forum for the most important issues facing our region with locally produced, topical documentaries; partners with others to foster education for all ages; and utilizes digital technology and Web services to expand those valued services. Beginning with a single television transmitter in Fargo, Prairie Public Broadcasting has grown to become the premier broadcaster of public television and radio services throughout the prairie region.
+</p>
 
 			<h2>Online Collection</h2>
-			<p></p>
+			<p>As a partner in Digital Horizons, Prairie Public Broadcasting has included photographs of behind-the-scenes views of some of Prairie Public’s 50 years of locally produced programs.  Visit Prairie Public’s <a href="http://www.youtube.com/prairiepublicbcast">YouTube</a> channel to watch full length documentaries and short features about the history, arts, culture, and people of the prairie region.
+</p>
 
 			<h2>Collection Policy</h2>
-			<p></p>
+			<p>Prairie Public Broadcasting is not actively collecting from outside sources for its footage library. We urge those with home movies or private collections of film or video to consider donating them to the State Historical Society of North Dakota or to the NDSU Institute for Regional Studies. Films and videos taken depicting college life or events could be donated to the archives of the respective academic institution.
+</p>
 		</div>
 
 		<div class="landing-page-sidebar">
-			<a href="http://library.ndsu.edu/ndsuarchives/sites/default/files/ndsu-archives-order-form.pdf" class="search_content_button" id="order">Order Form</a>
-			<h3>Collections</h3>
-			<ul>
-				<li></li>
+
+			<h3>Collection</h3>
+			<ul><li><a href="http://cdm16921.contentdm.oclc.org/cdm/landingpage/collection/pp-store">Behind the Scenes</a></li>
 			</ul>
 
 			<h3>Contact Information</h3>
-			
+			Prairie Public Broadcasting <br>
+			Television Department  <br>
+			207 5th Street North <br>
+			Fargo, ND 58102<br>
+			 (701) 241-6900 extension 631<br>
+			<a href="http://www.prairiepublic.org">www.prairiepublic.org</a>
 
-			<h3>Ordering information</h3>
-
-			<h3>Donations</h3>
 		</div>
 	</div>
 </div>
